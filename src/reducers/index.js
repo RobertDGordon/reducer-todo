e6aa3b799f3c9ccccc259301df06d34d58029d97
Robert Gordon <Komeuppance@gmail.com>
2019-12-03 22:16:12 -0800

    Step 1 completed

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+export const intialState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 1
+};
+
+export const todoReducer = (state, action) => {
+    switch (action.type) {
+        case "SET_ITEM":
+          return { ...state, title: action.payload };
+        case "TOGGLE_COMPLETED":
+          return { ...state, completed: !state.completed };
+        default:
+          return state;
+      }
+}
